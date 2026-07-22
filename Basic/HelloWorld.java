928236f8cb8cf23176267e58791ccae15c5b08d4
Ismail Ansari <ansariismailyyy@gmail.com>
2026-07-22 10:09:03 +0530

    Revision Java Hello World

---
+++ Basic/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String name = "Ismail";
+        int age = 22;
+        String gender = "Male";
+        String stream = "Computer Science";
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My gender is " + gender);
+        System.out.println("My stream is " + stream);
+    }
+}
