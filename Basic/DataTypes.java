0e2fc1d22539f8960bc236cfacb2e0eba636abea
Ismail Ansari <ansariismailyyy@gmail.com>
2026-07-23 21:09:21 +0530

    Java DataTypes Revision

---
+++ Basic/DataTypes.java
@@ -0,0 +1,31 @@
+public class DataTypes {
+    public static void main(String[] args)  {
+        // Primitive Data Types
+        // Data Type      Memory Size
+        // 1. int         4 byte 
+        // 2. float        4 bytes
+        // 3. double       8 bytes
+        // 4. char         2 bytes
+        // 5. boolean      1 byte   
+        // 6. byte          1 byte
+        // 7. short         2 bytes
+        // 8. long          8 bytes
+
+        // Non Primitive Data Types
+        // 1. String
+        // 2. Array
+        // 3. Class
+        // 4. Object
+        // 5. Interface
+
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+
+        // d =  (a * b) / (a - b)
+
+        int d = (a * b) / (a - b);
+        System.out.println(d);
+    }
+}
