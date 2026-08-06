326aebae11d43882bf723aa2e47fcbfdee25f718
Ismail Ansari <ansariismailyyy@gmail.com>
2026-08-06 22:11:48 +0530

    Operators

---
+++ Operators/Operators.java
@@ -0,0 +1,98 @@
+public class Operators {
+    public static void main(String[] args) {
+        // Symbols that tell compiler to perform some operation
+        // binary operator perform operation between two operands
+
+        // Arithmatic Operators
+
+        // Binary
+        // + plus
+        // - minus
+        // * multiply
+        // / devide
+        // % modulo
+
+        // Unary
+        // ++ increment     
+        // -- decrement
+        // ++a pre-increment  first change the value then use 
+        // a++ post-increment first use the value the change
+
+        // Relational Operators
+        // ==
+        // !=
+        // >
+        // < 
+        // >=
+        // <=
+       
+       // Logical Operators
+       // && Logical AND
+       // || Logical OR
+       // !  Logical NOT
+
+       // BINARY NUMBER SYSTEM
+    
+        // 0 to 9 Decimal
+
+        // Decimal base 10
+        // Binary  base 2
+        // Octa    base 8
+        // Hexadecimal base 16
+
+        // Bitwise Operators
+        // & Bitwise AND
+        // | Bitwise OR
+        // ^ Bitwise XOR - similar value will return false else true.
+        // ~ Bitwise NOT - (complement) convert 1 to 0 and 0 to 1
+        // << Bitwise Left Shift
+        // >> Bitwise Right Shift
+
+        // & Bitwise AND
+        // a = 0101, b = 0110
+        //   0 0 1 1
+        // & 0 1 1 0
+        //   0 1 1 1
+
+        // | Bitwise OR
+        // a = 0101, b = 0110
+        //   0 0 1 1
+        // | 0 1 1 0
+        //   0 1 1 1
+
+        // ^ Bitwise XOR
+        // a = 0101, b = 0110
+        //   0 0 1 1
+        // ^ 0 1 1 0
+        //   0 1 0 1
+
+        // << Bitwise Left Shift
+        // a = 0101
+        //   0 0 1 1
+        // << 0 1 1 0
+
+        // >> Bitwise Right Shift
+        // a = 0101
+        //   0 0 1 1
+        // >> 0 1 1 0
+
+        // Assignment Operators
+        // =
+        // +=
+        // -=
+        // *=
+        // /=
+
+        // a = 10, b = 5
+        // a = b; // a = 5
+        // a += b; // a = a + b = 10 + 5 = 15 // addition perform first then assign 
+        // a -= b; // a = a - b = 15 - 5 = 10 // subtraction perform first then assign
+        // a *= b; // a = a * b = 10 * 5 = 50 // multiplication perform first then assign
+        // a /= b; // a = a / b = 50 / 5 = 10 // devide perform first then assign       
+       
+    }
+
+
+
+
+}
