4321d67a1fce80615cf49c569467da21dbf762c4
Ismail Ansari <ansariismailyyy@gmail.com>
2026-08-08 21:52:28 +0530

    Time & Space Complexity

---
+++ TimeSpaceComplexity/SpaceComplexity.java
@@ -0,0 +1,5 @@
+// space complexity depends on how much space taken by code and variables stored by the same.
+// space is constant for variables
+// space is variable for arrays datatype in this input size will increase memory size.
+
+
---
+++ TimeSpaceComplexity/TimeComplexity.java
@@ -0,0 +1,11 @@
+// Time Complexity
+
+// Relation between input size & running time (operations).
+
+// input size == running time (linear, quadratic, cubic, square root.)
+
+// time complexity is directly proportional to input n.
+
+// BEST CASE     omega (1)
+// AVERAGE CASE  theta (2n+1/2)
+// WORST CASE    O (n)
