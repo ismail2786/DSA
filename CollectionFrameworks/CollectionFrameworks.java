91d85f9ea6c41dfd31db83c745dd588c4e8ac5e7
Ismail Ansari <ansariismailyyy@gmail.com>
2026-08-29 23:02:10 +0530

    Java Collection Frameworks

---
+++ CollectionFrameworks/CollectionFrameworks.java
@@ -0,0 +1,27 @@
+// Collection of Classes & Interfaces
+// Iterable => Collection => List | Queue | Set
+
+// List Interface => ArrayList | LinkedList | Vector(Stack)
+
+// Queue Interface (FIFO) => PriorityQueue | LinkedList | Deque (ArrayDeque) (Double ended Queue = object will add from both sides.)
+
+// Set Interface => HashSet | LinkedHashSet | SortedSet (TreeSet)
+
+// Map Interface => HashMap | LinkedHashMap | SortedMap (TreeMap = value sorted according to key) | Hashtable  
+
+// Set & Map are very important.
+
+// Methods on Collections
+// add
+// size
+// remove
+// iterate
+// addAll = add multiple objects at a time
+// removeAll = remove multiple objects at a time
+// clear
+
+public class CollectionFrameworks {
+
+
+
+}
