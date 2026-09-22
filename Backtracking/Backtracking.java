73060f3ac9db7a50324e3e9d4036db760c66cf93
Ismail Ansari <ansariismailyyy@gmail.com>
2026-09-22 21:32:10 +0530

    Backtracking

---
+++ Backtracking/Backtracking.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Backtracking {
+    // public static void printPermutation(String str, String perm, int idx) {
+    //     if(str.length() == 0) {
+    //         System.out.println(perm);
+    //         return;
+    //     }
+    //     for(int i=0; i<str.length(); i++) {
+    //         char currChar = str.charAt(i);
+    //         String newStr = str.substring(0,i) + str.substring(i+1);
+
+    //         printPermutation(newStr, perm+currChar, idx+1);
+    //     }
+    // }
+}
